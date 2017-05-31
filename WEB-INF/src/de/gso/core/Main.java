6bb54a0b14f42042c8572b64a6608b3c84d25950
BuildTools <unconfigured@null.spigotmc.org>
2017-05-31 21:23:08 +0200

    Main Methode DS

--- WEB-INF/src/de/gso/core/Main.java
+++ WEB-INF/src/de/gso/core/Main.java
@@ -2,7 +2,10 @@ package de.gso.core;
 
 public class Main {
 
+	/* run() wird nach Tomcat Server Start ausgeführt.
+	 * Und dient als Startschuss für die Applikation.
+	 */
 	public void run() {
-		System.out.println("TEST");
+		
 	}
 }
